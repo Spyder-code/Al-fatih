1b954ad98b3352ea516198fce77525f4273e65f1
Spyder-code <luaysyauqillah@gmail.com>
2020-01-20 14:00:13 +0700

    Memperbaiki

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -20,7 +20,7 @@
                     <p>Setelah anda melakukan transaksi akan ada konfirmasi pembayaran, anda dapat
                         membayar atas persetujuan bersama atau anda dapat langsung berkunjung ke toko kami.
                     </p>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.9383687307532!2d112.42210202913911!3d-7.49244847000745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7813086548b7a5%3A0x958432f3cc11b80f!2sToko%20sepatu%20Al%20fatih!5e0!3m2!1sen!2sid!4v1579158688351!5m2!1sen!2sid" width="430" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.9383687307532!2d112.42210202913911!3d-7.49244847000745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7813086548b7a5%3A0x958432f3cc11b80f!2sToko%20sepatu%20Al%20fatih!5e0!3m2!1sen!2sid!4v1579158688351!5m2!1sen!2sid" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                 </div>
             </div>
         </div>
